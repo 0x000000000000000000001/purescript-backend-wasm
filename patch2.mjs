a4ff5ae1bb5b78a6fb308e709268be9d04e7ce22
0x1 <kevin.francart3@gmail.com>
2026-08-07 22:44:42 +0200

    misc

--- patch2.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-const f = 'compiler/src/PureScript/Backend/Wasm/Lower/Unbox.purs';
-let content = fs.readFileSync(f, 'utf8');
-content = content.replace(
-  "  unboxModule m = m { decls = map (unboxBind (deriveSigs m.decls)) m.decls }",
-  "  unboxModule m = let sigs = deriveSigs m.decls in let _ = unsafePerformEffect (Console.log (show (Map.toUnfoldable sigs :: Array (Tuple String Sig)))) in m { decls = map (unboxBind sigs) m.decls }"
-);
-fs.writeFileSync(f, content);
